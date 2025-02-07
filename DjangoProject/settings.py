2fe5ea3607c26fc9cd7e662e56d5ff2a843f230c
Mehroj <mekhrojmajidov@gmail.com>
2025-02-07 08:09:42 +0500

    Set up Postgresql database for Django

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'DjangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myapp',
+        'USER': 'postgres',
+        'PASSWORD': '1649',
+        'HOST': 'localhost',
     }
 }
 
